d34ab06f926c724bda03ef9a10c81f94f28ab7b3
nqv1208 <qvinhkl10@gmail.com>
2026-06-29 04:26:33 +0700

    fix(ci): clean format

--- backend/tests/Notrelix.API.Tests/Contracts/NotrelixApiFactory.cs
+++ backend/tests/Notrelix.API.Tests/Contracts/NotrelixApiFactory.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
